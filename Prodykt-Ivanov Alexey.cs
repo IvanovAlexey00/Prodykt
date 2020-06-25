26cf5b8b880fe1b92840d782049713d3a2045589
IvanovAlexey00 <63222507+IvanovAlexey00@users.noreply.github.com>
2020-06-25 13:58:43 +0400

    Add files via upload

---
+++ Prodykt-Ivanov Alexey.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodykt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Molochka Prodykt = new Molochka("кефир", 7);
+
+            Prodykt.MolochkaMethod();
+
+
+            Console.Read();
+        }
+    }
+
+    class Molochka
+    {
+        protected string Name { get; set; }
+        protected int Shelflife { get; set; }
+
+        public Molochka(string name, int Shelflife)
+        {
+            Name = name;
+            Shelflife = Shelflife;
+            Console.WriteLine("Родительский класс создан");
+        }
+
+        public virtual void MolochkaMethod()
+        {
+            Console.WriteLine("Кефир: {0} может храниться в холодильнике 14 дней ", Name, Shelflife);
+        }
+
+
+    }
+
+    class Prodykt : Molochka
+    {
+        public Prodykt(string name, int Shelflife) : base(name, Shelflife)
+        {
+            Name = name;
+            Shelflife = Shelflife;
+            Console.WriteLine("Дочерний класс создан");
+        }
+
+        public override void MolochkaMethod()
+        {
+            Console.WriteLine("Молочка: {0} имеет срок годности {1} дней", Name, Shelflife);
+        }
+    }
+}
